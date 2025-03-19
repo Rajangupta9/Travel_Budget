e8e10d9dc57a310557e8da239af010cbe751bf62
Rajan gupta <81641542+Rajangupta9@users.noreply.github.com>
2025-03-19 18:07:12 +0530

    Add CORS middleware and update .gitignore to include package-lock.json

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const connectdb = require('./Config/db')
 const userRouter = require('./Routes/userRoutes')
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
