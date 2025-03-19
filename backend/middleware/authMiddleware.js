33ab99e66784420b2b116764981bf021ef803722
Rajan gupta <81641542+Rajangupta9@users.noreply.github.com>
2025-03-19 17:58:31 +0530

    Add authentication middleware to verify JWT tokens

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req,res,next)=>{
+    const token = req.header.authorization?.split(" ")[1];
+    if(!token) return res.status(401).json({msg: "Access Denied"});
+    jwt.verify(token, process.env.ACCESS_SECRET_KEY, (err, decode)=>{
+        if(err) return res.status(403).json({msg: "Invalid Token"});
+        req.user = decode;
+        next();
+    });
+    
+}
+
+module.exports = verifyToken;
