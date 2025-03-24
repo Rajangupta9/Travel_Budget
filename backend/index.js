a234e43875d202d87e6ad6dd1c12ece9bc730aa6
Rajan gupta <81641542+Rajangupta9@users.noreply.github.com>
2025-03-24 07:30:33 +0530

    Add support for new access and refresh token headers in CORS configuration

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ app.use(
     origin: ["http://localhost:5173", "https://travel-budget-zeta.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
+    allowedHeaders: ["Content-Type", "Authorization" , "x-new-access-token" , "x-refresh-token"]
   })
 );
 
